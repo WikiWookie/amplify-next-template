a553d494e6b55e019627eb33ae438d2912f0a522
wiki wookie <coolwest@gmail.com>
2024-06-07 14:59:00 -0500

    add server side changes

---
+++ app/login/page.tsx
@@ -0,0 +1,6 @@
+import Login from "@/components/Login";
+
+// TODO: Test Custom <Authenticator> example -> https://docs.amplify.aws/nextjs/build-a-backend/server-side-rendering/nextjs-app-router-server-components/#build-ui
+export default function LoginPage() {
+  return <Login />;
+}
