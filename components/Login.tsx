cbbefd652aa897201d2827f5a55e17aa4208bfa2
WikiWookie <coolwest@gmail.com>
2024-06-10 19:47:18 -0500

    wip

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { View, Text, useTheme, withAuthenticator } from "@aws-amplify/ui-react";
+import { View, Text, useTheme, withAuthenticator, Heading, Button, useAuthenticator } from "@aws-amplify/ui-react";
 import { AuthUser } from "aws-amplify/auth";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 import '@aws-amplify/ui/dist/styles.css';
-
+import '../utils/hideAmplifyTabs.css';
+// TODO: Setup TOTP page??
 function Login({ user }: { user?: AuthUser }) {
   useEffect(() => {
     if (user) {
@@ -17,18 +18,55 @@ function Login({ user }: { user?: AuthUser }) {
 
 // Test component customizing for withAuthenticator
 const testComponents = {
-  Footer() {
-    const { tokens } = useTheme();
+  // SignIn() {},
+  // Footer() {
+  //   const { tokens } = useTheme();
 
-    return (
-      <View textAlign="center" padding={tokens.space.large}>
-        <Text color={tokens.colors.neutral[80]}>
-          &copy; All Rights Reserved TESTTT
-        </Text>
-      </View>
-    );
-  }
+  //   return (
+  //     <View textAlign="center" padding={tokens.space.large}>
+  //       <Text color={tokens.colors.neutral[80]}>
+  //         &copy; All Rights Reserved TESTTT
+  //       </Text>
+  //     </View>
+  //   );
+  // }
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      // TODO: Add image at top
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Login
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toForgotPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toForgotPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
 };
 
 // export default withAuthenticator(Login); // Not customized
-export default withAuthenticator(Login, {components: testComponents }); // Customized
+export default withAuthenticator(
+  Login, 
+  {
+    initialState: 'signIn',
+    components: testComponents 
+  }
+); // Customized
